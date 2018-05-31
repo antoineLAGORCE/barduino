0fa1a4a474af4166591ffdffd41147f5193a17b8
DorianARNOUX-PROST <34739746+DorianARNOUX-PROST@users.noreply.github.com>
2018-05-31 12:06:31 +0200

    Add files via upload

--- doc/code/Version_finale.ino
+++ doc/code/Version_finale.ino
@@ -1,6 +1,6 @@
 // RX 7
 // TX 6
-//Servo 8
+//Servo 8, 13
 // Moteur CC 9,10,11,12
 
 
@@ -13,6 +13,7 @@ SoftwareSerial BTSerie(RxD,TxD);
 
 #include <Servo.h>
 Servo myservo;
+Servo myservo2;
 
 int Paso [ 8 ][ 4 ] 
    {  {1, 0, 0, 0},
@@ -30,6 +31,8 @@ int Paso [ 8 ][ 4 ]
 #define IN3  10
 #define IN4  9
 
+
+
 int steps_left=4095/6;
 boolean Direction = true;
 int Steps = 0; // Define el paso actual de la secuencia
@@ -41,8 +44,11 @@ void setup(){
      pinMode(IN3, OUTPUT);
      pinMode(IN4, OUTPUT);
      Serial.begin(9600);
-      myservo.attach(8);  // attaches the servo on pin 8 to the servo object
-     
+      myservo.attach(8,0,180);  // attaches the servo on pin 8 to the servo object
+      myservo.write(110);
+      myservo2.attach(13,0,180);
+      myservo2.write(0);
+  
   pinMode(RxD, INPUT);  
   pinMode(TxD, OUTPUT); 
   BTSerie.begin(9600); 
@@ -51,34 +57,39 @@ void setup(){
   
   BTSerie.print("AT+VERSION");  //Demande le NÂ° de version 
   delay(1000);  
-   
+
    }
 
 void loop()
       {
 
-
   
- char recvChar;  
+ String recvChar;  
   //On lit caractere par caractere sur le BTSerie et on affiche sur le Terminal Serie  
   if (BTSerie.available()) {  //Ces fonctions moteurCC sont valables pour le moteur avec la tige vers le haut
-    recvChar = BTSerie.read();  
-    if(recvChar == '1') { //Vodka cranberry
+    recvChar = BTSerie.readString();  
+    
+ 
+
+    
+    if(recvChar == "vodka cranberry") { //Vodka cranberry
       servoMoteur();
-      moteurCC2();
+      moteurCC1();
       delay(1000);
       servoMoteur();
       servoMoteur();
       servoMoteur();
-      moteurCC2();
+      moteurCC_2();
       delay(1000);
       servoMoteur();
-      moteurCC2();
+      moteurCC1();
       delay(1000);
+      Serial.println("Voila un vodka cranberry");
       
+      }
+
       
-    }
-    if(recvChar == '2') { //Bay Breeze
+    if(recvChar == "Bay breeze") { //Bay Breeze
       servoMoteur();
       moteurCC1();
       delay(1000);
@@ -88,9 +99,10 @@ void loop()
       servoMoteur();
       moteurCC_2();
       delay(1000);
+      Serial.println("Voila un bay breeze");
     }
 
-    if(recvChar == '3') { //Hole in one
+    if(recvChar == "all in one") { //Hole in one
       servoMoteur();
       moteurCC2();
       delay(1000);
@@ -99,43 +111,46 @@ void loop()
       moteurCC3();
       delay(1000);
       servoMoteur();
-      moteurCC1();
+      moteurCC_5();
       delay(1000);
+      Serial.println("Voila un Hole in one");
     }
 
 
-    if(recvChar == '4') { //Hoola Hoop
+    if(recvChar == "hula hoop") { //Hoola Hoop
       moteurCC_1();
       delay(1000);
       servoMoteur();
-      moteurCC_2();
+      moteurCC2();
       delay(1000);
       servoMoteur();
-      moteurCC_2();
+      moteurCC2();
       delay(1000);
       servoMoteur();
-      moteurCC_1();
+      moteurCC_3();
       delay(1000);
+      Serial.println("Voila un Hoola Hoop");
     }
 
 
-    if(recvChar == '5') { //Gimlet
-      moteurCC_2();
-      delay(1000);
-      servoMoteur();
-      moteurCC_1();
-      delay(1000);
-      servoMoteur();
-      servoMoteur();
+    if(recvChar == "gimlet") { //Gimlet
       moteurCC3();
       delay(1000);
+      servoMoteur();
+      servoMoteur();
+      moteurCC1();
+      delay(1000);
+      servoMoteur();
+      moteurCC_4();
+      delay(1000);
+      Serial.println("Voila un gimlet");
     }
 
 
 
 
 
-    if(recvChar == '6') { //Screwdriver
+    if(recvChar == "screwdriver") { //Screwdriver
       servoMoteur();
       moteurCC_1();
       delay(1000);
@@ -144,10 +159,11 @@ void loop()
       servoMoteur();
       moteurCC1();
       delay(1000);
+     Serial.println("Voila un screwdriver");
     }
 
 
-    if(recvChar == '7') { //Supreme
+    if(recvChar == "supreme") { //Supreme
       servoMoteur();
       moteurCC1();
       delay(1000);
@@ -164,22 +180,24 @@ void loop()
       moteurCC1();
       delay(1000);
       servoMoteur();
-      moteurCC1();
+      moteurCC_5 ();
       delay(1000);
+      Serial.println("Voila un supreme");
     }
 
 
 
     
-    Serial.print(recvChar);  //ecriture
+    Serial.println(recvChar);  //ecriture
   }
   //On lit caractere par caractere sur le terminal et on affiche sur le BT Serie 
   if (Serial.available()) {  
     recvChar = Serial.read(); //lecture
-    BTSerie.write(recvChar);  //ecriture
+   // BTSerie.write(recvChar);  //ecriture
   }
 
-          
+
+
 
 }
 
@@ -213,13 +231,15 @@ void SetDirection()
    }
 
 void servoMoteur(){
-   for (int position=90; position<=145; position ++){
+   for (int position=110; position<=165; position ++){
   myservo.write(position);
+  myservo2.write(position*1.2-110);
   delay(15);
-  if (position==145){delay(2000);}
+  if (position==165){delay(2000);}
 }
-for (int position=145; position>=90; position --){
+for (int position=165; position>=110; position --){
   myservo.write(position);
+  myservo2.write(position*1.2-110);
   delay(15);
 }
 }
@@ -230,7 +250,7 @@ void moteurCC1(){
       stepper();
       delay(1);
     }
-    Serial.println(x);
+
     delay(0);
   }
 }
@@ -242,7 +262,7 @@ void moteurCC2(){
       stepper();
       delay(1);
     }
-    Serial.println(x);
+
     delay(0);
   }
 }
@@ -254,7 +274,30 @@ void moteurCC3(){
       stepper();
       delay(1);
     }
-    Serial.println(x);
+
+    delay(0);
+  }
+}
+
+void moteurCC4(){
+  for(int x=0; x<4;x++){
+    for (int steps_left = 4095/6;  steps_left>0;  steps_left--){
+      stepper();
+      delay(1);
+    }
+
+    delay(0);
+  }
+}
+
+
+void moteurCC5(){
+  for(int x=0; x<5;x++){
+    for (int steps_left = 4095/6;  steps_left>0;  steps_left--){
+      stepper();
+      delay(1);
+    }
+
     delay(0);
   }
 }
@@ -266,7 +309,7 @@ void moteurCC_2(){
       stepper_();
       delay(1);
     }
-    Serial.println(x);
+
     delay(0);
   }
 }
@@ -277,7 +320,44 @@ void moteurCC_1(){
       stepper_();
       delay(1);
     }
-    Serial.println(x);
+
+    delay(0);
+  }
+}
+
+
+void moteurCC_3(){
+  for(int x=0; x<3;x++){
+    for (int steps_left = 4095/6;  steps_left>0;  steps_left--){
+      stepper_();
+      delay(1);
+    }
+
+    delay(0);
+  }
+}
+
+
+
+void moteurCC_4(){
+  for(int x=0; x<4;x++){
+    for (int steps_left = 4095/6;  steps_left>0;  steps_left--){
+      stepper_();
+      delay(1);
+    }
+
+    delay(0);
+  }
+}
+
+
+void moteurCC_5(){
+  for(int x=0; x<5;x++){
+    for (int steps_left = 4095/6;  steps_left>0;  steps_left--){
+      stepper_();
+      delay(1);
+    }
+
     delay(0);
   }
 }
